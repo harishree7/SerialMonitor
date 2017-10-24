211ef26d4aa7ff93c962c0065c4224d690b2f633
xeecos <flashindream@gmail.com>
2017-10-24 18:01:34 +0800

    update

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -4,124 +4,121 @@ var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var BomPlugin = require("webpack-utf8-bom");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
-module.exports = [{
-        devtool: "source-map",
-        entry: {
-            bundle: "./src-new/browser/ui/entry.js"
-        },
-        output: {
-            path: path.resolve(__dirname, "./dist/browser/html"),
-            filename: "[name].js",
-            sourceMapFilename: "bundle.map"
-        },
-        resolve: {
-            extensions: [".js", ".jsx"]
-        },
-        module: {
-            loaders: [{
-                    test: /\.(js|jsx)$/,
-                    loader: "babel-loader",
-                    include: [path.resolve(__dirname, "src")],
-                    exclude: [/node_modules/, "./src/electron-main", "./src/platform"],
-                    query: {
-                        presets: ["babel-preset-es2015", "babel-preset-react"],
-                        plugins: [
-                            [
-                                require("babel-plugin-import"),
-                                { libraryName: "antd", style: "css" }
-                            ]
-                        ]
-                    }
-                },
-                {
-                    test: /\.(png|jpg|gif|svg)$/,
-                    loader: "file-loader",
-                    query: {
-                        name: "assets/[name].[ext]?[hash]"
-                    }
-                },
-                {
-                    test: /\.(eot|ttf|svg|woff2?)(\?.*)?$/,
-                    loader: "url-loader?limit=100&name=fonts/[name].[ext]"
-                },
-                {
-                    test: /\.(css|scss)$/,
-                    loaders: ["style-loader", "css-loader", "sass-loader"]
-                },
-                {
-                    test: /\.(htm|html)$/,
-                    loader: "html-loader",
-                    query: {
-                        name: "[name].[ext]?[hash]"
-                    }
-                },
-                {
-                    test: /\.node$/,
-                    loader: "node-loader"
-                }
-            ]
-        },
-        target: "electron",
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: "src/browser/templates/index.html",
-                inject: false
-            }),
-            new HtmlWebpackPlugin({
-                title: "error",
-                filename: "error.html",
-                template: "src/browser/templates/error.ejs",
-                inject: false
-            }),
-            new BomPlugin(true, /\.(js|jsx)$/),
-            new CopyWebpackPlugin([{
-                    from: path.resolve("src-new/browser", "assets"),
-                    to: path.resolve("dist/browser/html/assets")
-                },
-                {
-                    from: path.resolve("src-new/browser", "i18n", "locales"),
-                    to: path.resolve("dist/browser/html/assets", "locales")
-                }
-            ])
-        ]
+module.exports = [
+  {
+    devtool: "source-map",
+    entry: {
+      bundle: "./web/entry.js"
     },
-    {
-        entry: "./src/electron-main/main.js",
-        output: {
-            path: path.resolve(__dirname, "./dist/electron-main"),
-            publicPath: "/dist/",
-            filename: "main.js"
+    output: {
+      path: path.resolve(__dirname, "./bundle/web/"),
+      filename: "[name].js",
+      sourceMapFilename: "bundle.map"
+    },
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.(js|jsx)$/,
+          loader: "babel-loader",
+          include: [path.resolve(__dirname, "src")],
+          exclude: [/node_modules/, "./app"],
+          query: {
+            presets: ["babel-preset-env", "babel-preset-react"],
+            plugins: [
+              [
+                require("babel-plugin-import"),
+                { libraryName: "antd", style: "css" }
+              ]
+            ]
+          }
         },
-        target: "electron",
-        module: {
-            exprContextCritical: true,
-            wrappedContextCritical: true,
-            wrappedContextRecursive: true,
-            wrappedContextRegExp: /^\.\//,
-            exprContextRegExp: /^\.\//,
-            unknownContextRegExp: /^\.\//,
-            loaders: [{
-                test: /\.js$/,
-                loader: "babel-loader",
-                include: [path.resolve(__dirname, "src/electron-main")],
-                exclude: [/node_modules/, /extensions/]
-            }],
-            rules: [{
-                test: /\.node$/,
-                use: "node-loader"
-            }]
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: "file-loader",
+          query: {
+            name: "assets/[name].[ext]?[hash]"
+          }
         },
-        resolve: {
-            modules: [path.resolve("./node_modules")]
+        {
+          test: /\.(eot|ttf|svg|woff2?)(\?.*)?$/,
+          loader: "url-loader?limit=100&name=fonts/[name].[ext]"
         },
-        externals: {
-            sqlite3: "commonjs sqlite3",
-            serialport: "commonjs serialport"
+        {
+          test: /\.(css|scss)$/,
+          loaders: ["style-loader", "css-loader", "sass-loader"]
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                $dirname: "__dirname"
-            })
-        ]
-    }
+        {
+          test: /\.(htm|html)$/,
+          loader: "html-loader",
+          query: {
+            name: "[name].[ext]?[hash]"
+          }
+        },
+        {
+          test: /\.node$/,
+          loader: "node-loader"
+        }
+      ]
+    },
+    target: "electron",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "web/templates/index.html",
+        inject: false
+      }),
+      new BomPlugin(true, /\.(js|jsx)$/),
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve("web", "assets"),
+          to: path.resolve("bundle/web/assets")
+        }
+      ])
+    ]
+  },
+  {
+    entry: "./app/app.js",
+    output: {
+      path: path.resolve(__dirname, "./bundle/app"),
+      publicPath: "/bundle/",
+      filename: "app.js"
+    },
+    target: "electron",
+    module: {
+      exprContextCritical: true,
+      wrappedContextCritical: true,
+      wrappedContextRecursive: true,
+      wrappedContextRegExp: /^\.\//,
+      exprContextRegExp: /^\.\//,
+      unknownContextRegExp: /^\.\//,
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          include: [path.resolve(__dirname, "app")],
+          exclude: [/node_modules/, /extensions/]
+        }
+      ],
+      rules: [
+        {
+          test: /\.node$/,
+          use: "node-loader"
+        }
+      ]
+    },
+    resolve: {
+      modules: [path.resolve("./node_modules")]
+    },
+    externals: {
+      sqlite3: "commonjs sqlite3",
+      serialport: "commonjs serialport"
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        $dirname: "__dirname"
+      })
+    ]
+  }
 ];
